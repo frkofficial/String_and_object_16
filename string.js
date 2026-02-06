6913a45123ec5077241eb9d00f764767ff04d9ec
frkofficial <farukakram2000@gmail.com>
2026-02-06 10:00:23 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,12 @@
+const country = 'BD';
+const division = "Dhaka";
+const district = `Chittagong`;
+
+
+
+console.log(district);
+
+
+// string is immutablen -- - > change is possible
+
+// array is mutable 
---
+++ string_comparison.js
@@ -0,0 +1,17 @@
+const school = 'Nasirabad boys';
+
+
+const subject = 'Biology';
+const book = 'biology';
+
+
+if(subject.toLocaleLowerCase == book.toLocaleLowerCase){
+    console.log("xm a kopai dibo");
+}
+else{
+    console.log("nahole parbo na")
+}
+
+
+console.log(book.toLowerCase());
+console.log(book.toLocaleUpperCase());
